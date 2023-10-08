d4b87001b2cd41d1c60c10bcc8f3ff6bb445a1fa
tonya-Diagnal <141700782+tonya-diagnal@users.noreply.github.com>
2023-10-08 14:59:10 +0530

    Add a user slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,10 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 // import movieListSlice from "./movieList/movieListSlice";
 import movieListSlice2 from "./movieList/movieListSlice2";
 import { useDispatch } from "react-redux";
+import userReducer from "./user/userSlice";
 
 const store = configureStore({
     // reducer: { movieList: movieListSlice.reducer },
-    reducer: { movieList: movieListSlice2.reducer },
+    reducer: { movieList: movieListSlice2.reducer, user: userReducer },
 });
 
 export default store;
---
+++ src/store/user/userSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+const initialState = { isLoggedIn: false, user: { email: "", password: "" } };
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        signInUser(state, action) {
+            const { email, password } = action.payload;
+            localStorage.setItem("user", JSON.stringify(action.payload));
+            localStorage.setItem("isLoggedIn", "true");
+            state.isLoggedIn = true;
+            state.user.email = email;
+            state.user.password = password;
+        },
+
+        signOutUser(state) {
+            console.log("in signout reducer");
+            localStorage.setItem("isLoggedIn", "false");
+            localStorage.removeItem("user");
+            state.isLoggedIn = false;
+            state.user.email = "";
+            state.user.password = "";
+        },
+    },
+});
+
+export const { signInUser, signOutUser } = userSlice.actions;
+export default userSlice.reducer;
+export const getUser = (state: RootState) => state.user.user;
+export const isLoggedIn = (state: RootState) => state.user.isLoggedIn;
