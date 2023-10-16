4c27820acb0a4a526714767e8b36d6321a026f86
tonya-Diagnal <141700782+tonya-diagnal@users.noreply.github.com>
2023-10-16 18:03:42 +0530

    Show as logged in on login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,10 +3,11 @@ import { FormEvent, useState } from "react";
 import styles from "./Login.module.css";
 import { CgProfile } from "react-icons/cg";
 import { useDispatch, useSelector } from "react-redux";
-import { getUser, signInUser } from "../../store/user/userSlice";
+import { getUser, isLoggedIn, signInUser } from "../../store/user/userSlice";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const isSignedIn = useSelector(isLoggedIn);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const user = useSelector(getUser);
@@ -16,12 +17,17 @@ const Login = () => {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
         const formDataObj = Object.fromEntries(formData.entries());
-        console.log(formDataObj);
+        // console.log(formDataObj);
         if (
             formDataObj["email"] === "test@diagnal.com" &&
             formDataObj["password"] === "123"
         ) {
-            dispatch(signInUser(formDataObj));
+            dispatch(
+                signInUser({
+                    email: formDataObj["email"],
+                    password: formDataObj["password"],
+                })
+            );
             navigate("/");
             // navigate("")
         } else {
@@ -31,49 +37,67 @@ const Login = () => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.card}>
-                <div className={styles.icon}>
-                    <CgProfile
-                        // onClick={handleOpen}ss
-                        className={styles.profileIcon}
-                        size={65}
-                        color="white"
-                    />
+            {isSignedIn ? (
+                <div className={styles.card}>
+                    <div className={styles.icon}>
+                        <CgProfile
+                            // onClick={handleOpen}ss
+                            className={styles.profileIcon}
+                            size={65}
+                            color="white"
+                        />
+                    </div>
+                    <div style={{ color: "white" }}>You are signed in</div>
                 </div>
-                <div className={styles.details}>
-                    <p className={styles.title}>
-                        <b>Login</b>
-                    </p>
-                    <div className={styles.description}>
-                        <p>Enter your credentials</p>
+            ) : (
+                <div className={styles.card}>
+                    <div className={styles.icon}>
+                        <CgProfile
+                            // onClick={handleOpen}ss
+                            className={styles.profileIcon}
+                            size={65}
+                            color="white"
+                        />
                     </div>
+                    <div className={styles.details}>
+                        <p className={styles.title}>
+                            <b>Login</b>
+                        </p>
+                        <div className={styles.description}>
+                            <p>Enter your credentials</p>
+                        </div>
+                    </div>
+                    <form className={styles.form} onSubmit={formSubmitHandler}>
+                        <div className={styles.formField}>
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" id="email" />
+                        </div>
+                        <div className={styles.formField}>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                            />
+                        </div>
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "row",
+                                justifyContent: "space-between",
+                                alignItems: "center",
+                            }}
+                        >
+                            <button type="submit">Login</button>
+                            {loginError && (
+                                <div className={styles.status}>
+                                    Invalid credentials
+                                </div>
+                            )}
+                        </div>
+                    </form>
                 </div>
-                <form className={styles.form} onSubmit={formSubmitHandler}>
-                    <div className={styles.formField}>
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="email" />
-                    </div>
-                    <div className={styles.formField}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password" />
-                    </div>
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            justifyContent: "space-between",
-                            alignItems: "center",
-                        }}
-                    >
-                        <button type="submit">Login</button>
-                        {loginError && (
-                            <div className={styles.status}>
-                                Invalid credentials
-                            </div>
-                        )}
-                    </div>
-                </form>
-            </div>
+            )}
         </div>
     );
 };
