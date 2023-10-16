8af2ea6fff277713a156fce5c5eded3d98e23102
tonya-Diagnal <141700782+tonya-diagnal@users.noreply.github.com>
2023-10-16 18:04:24 +0530

    Change design of global error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,9 +1,11 @@
+import styles from "./ErrorPage.module.css";
+
 const ErrorPage = () => {
-  return (
-    <div>
-      <h2>Could not find page.</h2>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <h1>Could not find page.</h1>
+        </div>
+    );
 };
 
 export default ErrorPage;
