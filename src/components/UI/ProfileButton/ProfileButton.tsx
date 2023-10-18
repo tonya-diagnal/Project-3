158176adf545b0eb0c9cfc3f1e57367428274c86
tonya-Diagnal <141700782+tonya-diagnal@users.noreply.github.com>
2023-10-18 17:15:31 +0530

    Add an alert when signing out.

--- src/components/UI/ProfileButton/ProfileButton.tsx
+++ src/components/UI/ProfileButton/ProfileButton.tsx
@@ -36,6 +36,7 @@ const ProfileButton = ({
     const logoutClickHandler = () => {
         handleProfileOpen(false);
         dispatch(signOutUser());
+        alert("Successfully signed out");
         navigate("/login");
     };
 
